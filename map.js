97e104d088acd86ff5a2c81c6ea4b3b9e8ab9cf0
aidan-mccarthy <amccarthy.business@gmail.com>
2019-07-19 09:07:41 -0400

    Map Updates
    [#] Fix canvas display
    [#] Fix map sizing
    [+] Add map zoom

--- map.js
+++ map.js
@@ -1,15 +1,46 @@
 var canvas = new fabric.Canvas('map_canvas', {
   selectionColor: 'blue',
   selectionLineWidth: 1,
-  moveCursor: 'move'
+  moveCursor: 'move',
+  width: window.innerWidth * 0.675,
+  height: window.innerHeight * 0.65
 });
 
-// Resize canvas to fill div
-window.addEventListener('resize', resizeCanvas, false);
-function resizeCanvas() {
-  canvas.setWidth = window.innerWidth * 0.675;
-  canvas.setHeight = window.innerHeight * 0.65;
-}
+// Set mouse wheel zoom
+canvas.on('mouse:wheel', function(opt) {
+  var delta = opt.e.deltaY;
+  var zoom = canvas.getZoom();
+
+  zoom = zoom - delta/200;
+  if (zoom > 20) zoom = 20;
+  if (zoom < 0.1) zoom = 0.1;
+
+  canvas.zoomToPoint({
+    x: opt.e.offsetX,
+    y: opt.e.offsetY
+  }, zoom);
+
+  opt.e.preventDefault();
+  opt.e.stopPropagation();
+
+  var vpt = this.viewportTransform;
+
+  if (zoom < 400 / 1000) {
+    this.viewportTransform[4] = 200 - 1000 * zoom / 2;
+    this.viewportTransform[5] = 200 - 1000 * zoom / 2;
+  } else {
+    if (vpt[4] >= 0) {
+      this.viewportTransform[4] = 0;
+    } else if (vpt[4] < canvas.getWidth() - 1000 * zoom) {
+      this.viewportTransform[4] = canvas.getWidth() - 1000 * zoom;
+    }
+    if (vpt[5] >= 0) {
+      this.viewportTransform[5] = 0;
+    } else if (vpt[5] < canvas.getHeight() - 1000 * zoom) {
+      this.viewportTransform[5] = canvas.getHeight() - 1000 * zoom;
+    }
+  }
+});
 
 // Add map to canvas
 fabric.Image.fromURL('/map.jpg', (img) => {
@@ -17,7 +48,8 @@ fabric.Image.fromURL('/map.jpg', (img) => {
     left: 0,
     top: 0
   });
-  img.scaleToHeight(100);
-  img.scaleToWidth(100);
+  img.scaleToWidth(canvas.getWidth());
   canvas.add(img);
 });
+
+canvas.renderAll();
